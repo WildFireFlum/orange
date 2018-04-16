e995f9b9d42c265367bbab5a9497419bec93af68
tzlil <tzlil.gon@gmail.com>
2018-04-16 20:02:50 +0300

    GaloisAllocator.h

--- galois/include/Galois/WorkList/kiwiqueue/GaloisAllocator.h
+++ galois/include/Galois/WorkList/kiwiqueue/GaloisAllocator.h
@@ -6,43 +6,23 @@
 template <uint32_t N=2>
 class GaloisAllocator : public Allocator {
 public:
-
     GaloisAllocator() : term(Runtime::getSystemTermination()) {}
 
     inline void* allocate(unsigned int numOfBytes, unsigned int listIndex) {
-        int e = term.getEpoch();
-        e %= 3;
-
-        if (listIndex == 0) {
-            return reinterpret_cast<void*>(chunkHeap[e].allocate(numOfBytes, listIndex));
-        }
-        return reinterpret_cast<void*>(roHeap[e].allocate(numOfBytes, listIndex));
+        return reinterpret_cast<void*>(heaps[listIndex][term.getEpoch() % 3].allocate(numOfBytes, 0));
     }
 
     inline void deallocate(void* ptr, unsigned int listIndex) {
-        int e = term.getEpoch();
-        e %= 3;
-        if (listIndex == 0) {
-            chunkHeap[e].deallocate(ptr, 0);
-        }
-        else {
-            roHeap[e].deallocate(ptr, 0);
-        }
+        heaps[listIndex][term.getEpoch() % 3].deallocate(ptr, 0);
     }
 
     inline void reclaim(void* ptr, unsigned int listIndex) {
-        int e = (term.getEpoch() + 2);
-        e %= 3;
-        if (listIndex == 0) {
-            chunkHeap[e].deallocate(ptr, 0);
-        }
-        else {
-            roHeap[e].deallocate(ptr, 0);
-        }
+        heaps[listIndex][(term.getEpoch() + 2)% 3].deallocate(ptr, 0);
     }
 
 private:
     // memory reclamation mechanism
+
     static Runtime::MM::ListNodeHeap heaps[N][3];
     Runtime::TerminationDetection& term;
 };
